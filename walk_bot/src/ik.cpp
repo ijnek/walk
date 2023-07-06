5609c8a449a81e01f11e376ae6c4052f5c829fe0
Kenji Brameld <kenjibrameld@gmail.com>
2023-07-06 03:08:20 +0000

    start getting ankle quaternion working
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

--- walk_bot/src/ik.cpp
+++ walk_bot/src/ik.cpp
@@ -14,6 +14,10 @@
 
 #include "walk_bot/ik.hpp"
 
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2/LinearMath/Matrix3x3.h"
+#include "tf2/LinearMath/Quaternion.h"
+
 namespace walk_bot
 {
 
@@ -43,6 +47,20 @@ void IK::ikCallback(const biped_interfaces::msg::SolePoses & msg)
   float l_phi = std::asin(-msg.l_sole.position.x / l_leg_length);
   float r_phi = std::asin(-msg.r_sole.position.x / r_leg_length);
 
+  tf2::Quaternion quat_l_ankle1;
+  // This is an issue (order should be yaw, pitch, roll), but the joint order is currently roll, pitch.
+  // Should swap joint orders such that they are pitch, roll.
+  quat_l_ankle1.setEuler(0, 0, 0);
+
+  tf2::Quaternion quat_left_sole;
+  tf2::fromMsg(msg.l_sole.orientation, quat_left_sole);
+
+  tf2::Quaternion quat_ankle = quat_left_sole * quat_l_ankle1.inverse();
+  tf2::Matrix3x3 rotation_quat_ankle(quat_ankle);
+  double roll, pitch, yaw;
+  rotation_quat_ankle.getEulerYPR(yaw, pitch, roll);
+  RCLCPP_INFO_STREAM(get_logger(), "yaw, pitch, roll: " << yaw << ", " << pitch << ", " << roll);
+
   joint_command.name.push_back("l_hip_roll");
   joint_command.position.push_back(l_theta);
 
@@ -55,28 +73,28 @@ void IK::ikCallback(const biped_interfaces::msg::SolePoses & msg)
   joint_command.name.push_back("r_hip_pitch");
   joint_command.position.push_back(r_phi);
 
-  joint_command.name.push_back("l_hip_yaw");
-  joint_command.position.push_back(0.0);
-
-  joint_command.name.push_back("r_hip_yaw");
-  joint_command.position.push_back(0.0);
-
   joint_command.name.push_back("l_leg_extension");
   joint_command.position.push_back(l_leg_length);
 
   joint_command.name.push_back("r_leg_extension");
   joint_command.position.push_back(r_leg_length);
 
-  joint_command.name.push_back("l_ankle_roll");
-  joint_command.position.push_back(0.0);
+  joint_command.name.push_back("l_ankle_yaw");
+  joint_command.position.push_back(yaw);
 
-  joint_command.name.push_back("r_ankle_roll");
+  joint_command.name.push_back("r_ankle_yaw");
   joint_command.position.push_back(0.0);
 
   joint_command.name.push_back("l_ankle_pitch");
-  joint_command.position.push_back(0.0);
+  joint_command.position.push_back(pitch);
 
   joint_command.name.push_back("r_ankle_pitch");
+  joint_command.position.push_back(0.0);
+
+  joint_command.name.push_back("l_ankle_roll");
+  joint_command.position.push_back(roll);
+
+  joint_command.name.push_back("r_ankle_roll");
   joint_command.position.push_back(0.0);
 
   pub_joint_command_->publish(joint_command);
